2e4d8d9e39e1029971a36cae152e33dba4599dda
AnneKok <annelindakok@gmail.com>
2014-04-04 14:35:04 +0100

    Behaviour added 04-04
    Behaviour class and subclasses (incomplete) added to the project

---
+++ src/group5/viope2014/Behaviour.java
@@ -0,0 +1,15 @@
+package group5.viope2014;
+
+/**
+ * Created by Anne on 04/04/14.
+ */
+public abstract class Behaviour {
+
+    // Fields
+    private int oldx,oldy;
+
+    //Methods
+    abstract public int getNewX();
+    abstract public int getNewY();
+
+}
---
+++ src/group5/viope2014/PacManBehaviour.java
@@ -0,0 +1,7 @@
+package group5.viope2014;
+
+/**
+ * Created by Anne on 04/04/14.
+ */
+public abstract class PacManBehaviour extends Behaviour {
+}
