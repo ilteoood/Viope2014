7f147495b497aa01347fedf02253bfb19710c779
Dzar87 <omgitsdzar@gmail.com>
2014-04-07 03:15:41 +0300

    Wall
    Still needs some work. Need to discuss what the plan is for the
    MazeElement class with Matteo and Jacopo.

--- src/group5/viope2014/Wall.java
+++ src/group5/viope2014/Wall.java
@@ -1,6 +1,25 @@
 package group5.viope2014;
 public class Wall extends MazeElement
 {
-	public Wall()
-	{}
+	public Wall(int x, int y) {
+        super.x = x;
+        super.y = y;
+    }
+
+    /* Overridden to disable movement of the Wall MazeElement. */
+    @Override public void move(int x, int y) {
+
+    }
+    /* As per the instructions for the Maze class. */
+    @Override public String toString() {
+        return "W";
+    }
+
+    /*Placeholders*/
+    public int getHorizontalOffset() {
+        return 0;
+    }
+    public int getVerticalOffset() {
+        return 0;
+    }
 }
