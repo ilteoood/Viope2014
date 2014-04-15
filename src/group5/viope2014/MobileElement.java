accc1a18d59b2e4a9fbdcd367f086bd593cbdc61
Kneas <leopold.mayoud@gmail.com>
2014-04-15 21:10:26 +0200

    15/04/ Added getter and setter for the MobileElement

--- src/group5/viope2014/MobileElement.java
+++ src/group5/viope2014/MobileElement.java
@@ -3,4 +3,14 @@ public abstract class MobileElement extends MazeElement
 {
 	// Fields
 	protected Behaviour bev;
+
+    //Sets the object's behaviour to b
+    public void setBehaviour(Behaviour b){
+        this.bev = b;
+    }
+
+    //Returns the object's behaviour
+    public Behaviour getBehaviour(){
+        return this.bev;
+    }
 }
