977accbc77543493b09d250774032002659a86c8
klaradvorak <klaradvorak71@gmail.com>
2014-05-13 20:54:01 +0200

    printMaze function added. Main modified accordingly.

--- src/group5/viope2014/Main.java
+++ src/group5/viope2014/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Maze field = new Maze("sometxtfile.txt");
         field.reload();
-        while (field.getLives() > 0) { //add more conditionals
-            field.move();
-        }
+        field.printMaze();
         System.exit(0);
 	// write your code here
 
