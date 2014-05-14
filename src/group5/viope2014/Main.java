8db23801cbc801f4141539c0fe26bf072e404fb8
Dzar87 <omgitsdzar@gmail.com>
2014-05-14 18:56:33 +0100

    Maze write() function working as intended

--- src/group5/viope2014/Main.java
+++ src/group5/viope2014/Main.java
@@ -5,6 +5,9 @@ public class Main
 
     public static void main(String[] args) {
     	Maze field = new Maze("filename.txt");
+    	System.out.println(field.toString());
+    	field.printMaze();
+    	field.write();
     	field.reload();
     	field.printMaze();
     	System.exit(0);
