347a0cbf7af674816345cb011e13c39502ff973d
Leopold Mayoud <leopold.mayoud8@etu.univ-lorraine.fr>
2014-05-20 16:55:51 +0200

    Updated AI MoveCloser

--- src/group5/viope2014/MoveCloser.java
+++ src/group5/viope2014/MoveCloser.java
@@ -3,6 +3,21 @@ package group5.viope2014;
 public class MoveCloser implements ArtificialIntelligence{
 	
 	public int intMove(int x, int y){
-		return 0;
+		int xD = Maze.eX - Maze.pcX;
+		int yD = Maze.eY - Maze.pcY;
+		int dir=-1;
+		if(xD > 0){
+			dir = choseDirection.Move_Left;
+		}
+		else if(xD < 0){
+			dir = choseDirection.Move_Right;
+		}
+		else if(yD > 0){
+			dir = choseDirection.Move_Up;
+		}
+		else if(yD < 0){
+			dir = choseDirection.Move_Down;
+		}
+		return dir;
 	}
 }
