5bfe6206df5ef2ca846d2955277e0c0403ca49a0
Jacopo <jacopo.delu@gmail.com>
2014-05-21 10:48:40 +0200

    Change AI behaviour name to real behaviour name.
    RandomMove -> FrightenedBehaviour
    MoveCloser -> ChaseBehaviour

--- src/group5/viope2014/ChaseBehaviour.java
+++ src/group5/viope2014/ChaseBehaviour.java
@@ -1,22 +1,22 @@
 package group5.viope2014;
 
-public class MoveCloser implements ArtificialIntelligence{
+public class ChaseBehaviour implements ArtificialIntelligence{
 	
 	public int intMove(int x, int y){
 		int xD = x - Maze.pcX;
 		int yD = y - Maze.pcY;
 		int dir=-1;
 		if(xD > 0){
-			dir = choseDirection.Move_Left;
-		}
-		else if(xD < 0){
-			dir = choseDirection.Move_Right;
-		}
-		else if(yD > 0){
 			dir = choseDirection.Move_Up;
 		}
-		else if(yD < 0){
+		else if(xD < 0){
 			dir = choseDirection.Move_Down;
+		}
+		else if(yD > 0){
+			dir = choseDirection.Move_Right;
+		}
+		else if(yD < 0){
+			dir = choseDirection.Move_Left;
 		}
 		return dir;
 	}
--- src/group5/viope2014/FrightenedBehaviour.java
+++ src/group5/viope2014/FrightenedBehaviour.java
@@ -1,7 +1,7 @@
 package group5.viope2014;
 import java.util.Random;
 
-public class RandomMove implements ArtificialIntelligence {
+public class FrightenedBehaviour implements ArtificialIntelligence {
 	
 	public int intMove(int x, int y){
 		Random r = new Random();
